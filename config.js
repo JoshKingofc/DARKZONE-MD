a1951e0593fc6145fe4fd44be5cca023bdcbe7e5
JoshKingofc <joshuamambozindoga04@gmail.com>
2025-09-29 07:02:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "IK~OlomhRYL#pIQW7oWW5iguUJOK6WpXdDIryPC4vE1tUpFYIPGeclY",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
